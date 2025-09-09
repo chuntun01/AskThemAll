6e30031987257fdfecb6bb64ecc02cc1c8bf919e
PHONG2155 <144548076+PHONG2155@users.noreply.github.com>
2025-09-09 18:13:11 +0700

    chore: remove accidentally tracked files from node_modules and .next

--- .next/static/chunks/app_statistics_page_tsx_0bd9f6c2._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_recharts_es6_9f56883d._.js",
-  "static/chunks/node_modules_d8ae6c0a._.js",
-  "static/chunks/app_6eb70443._.js"
-],
-    source: "dynamic"
-});
